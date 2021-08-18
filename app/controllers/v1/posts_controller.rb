4782e21cb343a3ffdfd79ab9bcf1de900757feb0
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-18 16:51:09 +0900

    詳細表示ページを実装

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,10 +1,13 @@
 class V1::PostsController < ApplicationController
+  before_action :set_post, only: %i[show update destroy]
+
   def index
     posts = Post.all
     render json: posts
   end
 
   def show
+    render json: @post
   end
 
   def create
@@ -14,5 +17,11 @@ class V1::PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
