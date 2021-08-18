966f91c92a1ad326a9160301e5a5b5e7e01b0026
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-18 16:47:51 +0900

    一覧表示に対して行うテストを作成

--- spec/requests/v1/posts_spec.rb
+++ spec/requests/v1/posts_spec.rb
@@ -2,6 +2,21 @@ require "rails_helper"
 
 RSpec.describe "V1::Posts", type: :request do
   describe "GET #index" do
+    context "投稿が存在するとき" do
+      subject { get(v1_posts_path) }
+      before { create_list(:post, 3) }
+
+      it "投稿一覧を取得できること" do
+        subject
+        json = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok)
+        expect(json.size).to eq 3
+        expect(json[0].keys).to eq ["id", "title", "content"]
+        expect(json[0]["id"]).to eq Post.first.id
+        expect(json[0]["title"]).to eq Post.first.title
+        expect(json[0]["content"]).to eq Post.first.content
+      end
+    end
   end
 
   describe "GET #show" do
